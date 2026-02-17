d968db11d78026f209d6fedb418b75e6d1a3a2d5
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2026-02-17 07:21:55 -0600

    empty is not an error case

--- dnsmasq_config/healthcheck.sh
+++ dnsmasq_config/healthcheck.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if [ -z "${PUB_HEALTH_CHECK_DOMAINS}" ]; then
+  echo "No domains to check"
+  exit 0
+fi
+
 for domain in ${PUB_HEALTH_CHECK_DOMAINS}; do
     echo "${domain}"
     dig -t txt "${domain}" @localhost
